renemilk/wxWidgets

commit 32bd17fcb5c038bb4e64ac1b849cbb8175f29cad

Define wxTextEntry::DoAutoCompleteStrings() stub in wxMSW code too.
Forgot to define this function in !HAS_AUTOCOMPLETE case. This fixes linking
problems with old compilers/headers such as VC6 with its original SDK.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@67518 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/textentry.cpp
@@ -844,6 +844,11 @@ bool wxTextEntry::DoAutoCompleteStrings(const wxArrayString& choices)
     return wxTextEntryBase::DoAutoCompleteStrings(choices);
 }
 
+bool wxTextEntry::DoAutoCompleteCustom(wxTextCompleter *completer)
+{
+    return wxTextCompleter::DoAutoCompleteCustom(completer);
+}
+
 #endif // HAS_AUTOCOMPLETE/!HAS_AUTOCOMPLETE
 
 #endif // wxUSE_OLE

commit 58b9573d28001e0876e30db928c636246481c571

Correct a typo in DoAutoCompleteCustom() stub in wxMSW.
Fix a compilation error introduced by r67518.

src/msw/textentry.cpp
@@ -825,7 +825,7 @@ bool wxTextEntry::DoAutoCompleteStrings(const wxArrayString& choices)
 
 bool wxTextEntry::DoAutoCompleteCustom(wxTextCompleter *completer)
 {
-    return wxTextCompleter::DoAutoCompleteCustom(completer);
+    return wxTextEntryBase::DoAutoCompleteCustom(completer);
 }
 
 #endif // HAS_AUTOCOMPLETE/!HAS_AUTOCOMPLETE